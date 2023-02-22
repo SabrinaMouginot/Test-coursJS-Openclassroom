fe3b9d87b4b2fc01745ad273df6a288f01d4b7f6
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-22 23:31:55 +0100

    declaration des variables

---
+++ TestJS.js
@@ -0,0 +1,5 @@
+let numberOfSeasons = 6;
+let numberOfEpisodes = 12;
+
+let paragraph = document.querySelector('#info');
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
