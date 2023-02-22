147b168cc57b6f17e1d6a4f428e0883f88053732
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-23 00:04:13 +0100

    calcul du  temps de visionnage total de cette série

--- TestJS.js
+++ TestJS.js
@@ -1,5 +1,21 @@
+// Create your variables here
+// ==========================================
+
 let numberOfSeasons = 6;
 let numberOfEpisodes = 12;
 
+// ==========================================
+
+// Calculate totalShowTime here
+// =====================================
+
+let episodeTime = 45;
+let commercialTime = 5;
+let totalShowTime = (episodeTime + commercialTime)*12*6;
+
+// =====================================
+
 let paragraph = document.querySelector('#info');
-paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season
+
+Total viewing time: ${totalShowTime} minutes`
